7ef4e508bae3781cc164edc7893cdd985569b403
kelv01 <kelvinnjugunanungari@gmail.com>
2025-05-25 22:57:19 +0300

    added language with for loop

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -13,6 +13,8 @@
 const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 
  for (let i = 0; i < lines.length; i++) {
-    console.log([i]);
+    console.log(lines[i]);
     
  }
+
+
