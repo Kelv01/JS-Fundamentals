0870041356e1a3f5fea13a9c08db66e98c79a05b
kelv01 <kelvinnjugunanungari@gmail.com>
2025-05-25 23:12:05 +0300

    factorial test

---
+++ 10-factorial.js
@@ -0,0 +1,9 @@
+const [, , arg] = process.argv;
+const num = Number(arg);
+
+function factorial(n) {
+  if (isNaN(n) || n <= 0) return 1;
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(num));
