0170ac4e8156da3edcc4684df2243e91e7b0ee3e
kelv01 <kelvinnjugunanungari@gmail.com>
2025-05-25 20:36:47 +0300

    added arguments statements

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+let arguments = 0;
+
+if (arguments <= 0) {
+    console.log('No argument');
+} if (arguments > 1) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
