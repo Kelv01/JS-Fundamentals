84ad9df023876bb22dbe6ce74c0a25bd0e4e4917
kelv01 <kelvinnjugunanungari@gmail.com>
2025-05-25 21:57:40 +0300

    added value argument

---
+++ 3-value_argument.js
@@ -0,0 +1,16 @@
+// Write a script that prints the first argument passed to it:
+
+// If no arguments are passed to the script, print “No argument”
+// You must use console.log(...) to print all output
+// You are not allowed to use var
+// You are not allowed to use length
+
+const firstAgr = process.avg[2];
+
+if (firstAgr === undefined) {
+    console.log('No argument');
+    
+} else {
+    console.log(firstAgr);
+}
+
