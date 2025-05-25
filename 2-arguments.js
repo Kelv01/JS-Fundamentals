34df35739a0c90e3ebb17a1829dbbf8b8d012c7e
kelv01 <kelvinnjugunanungari@gmail.com>
2025-05-25 21:33:46 +0300

    added arguments count

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,10 +1,11 @@
 let argsCount = process.argv.length - 2;
 
-if (arguments === 0) {
+if (argsCount === 0) {
     console.log('No argument');
-} if (arguments === 1) {
+} if (argsCount === 1) {
     console.log('Argument found');
 } else {
     console.log('Arguments found');
 }
+
 
